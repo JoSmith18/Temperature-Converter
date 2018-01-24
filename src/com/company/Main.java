73c2442497a60e5ddc0c46834265c82c5420b65d
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2018-01-24 10:55:34 -0600

    Changed the way the input is being took.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,32 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("What Is Your Starting Unit:");
-        System.out.println("[F]ahrenheit");
-        System.out.println("[C]elsius");
-        System.out.println("[K]elvin");
-
-        String startingUnit = input.nextLine().toUpperCase();
-
-        System.out.println("What Is Your New Unit:");
-        System.out.println("[F]ahrenheit");
-        System.out.println("[C]elsius");
-        System.out.println("[K]elvin");
-
-        String newUnit = input.nextLine().toUpperCase();
-
-        System.out.println("What is the temp?");
-
-        double temp = input.nextDouble();
-
+        System.out.println("Input ->");
+        String input1 = (input.next());
+        double temp = Double.valueOf(input1.substring(0, input1.length() - 1));
+        String startingUnit = Character.toString(input1.charAt(input1.length() - 1)).toUpperCase();
+        input.next();
+        String newUnit = input.next().toUpperCase();
         TemperatureConverter converter = new TemperatureConverter(startingUnit, newUnit);
-
         if (newUnit.equals("F")) {
-            System.out.println(converter.converter(temp) + " degrees Fahrenheit");
+            System.out.println("Output -> " + converter.converter(temp) + "F");
         } else if (newUnit.equals("C")) {
-            System.out.println(converter.converter(temp) + " degrees Celsius");
+            System.out.println("Output -> " + converter.converter(temp) + "C");
         } else if (newUnit.equals("K")) {
-            System.out.println(converter.converter(temp) + " degrees Kelvin");
+            System.out.println("Output -> " + converter.converter(temp) + "K");
         }
 
 
